8bd6819723db9c8e0b7fe4d6b202f216a3b144fe
John Dee <johndeebdd@gmail.com>
2021-03-08 17:55:19 -0800

    Asia working ccopy

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from "axios";
+import Character from './components/Character';
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+function App() {
+    const [userData,setUserData]=useState([]);
+    useEffect(()=>{
+        axios.get('https://swapi.dev/api/people/')
+          .then(resultJSON=>{
+              var numberOfResults = Object.keys(resultJSON).length;
+              console.log(numberOfResults);
+              console.log(JSON.stringify(resultJSON.data[1]));
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+              var character = resultJSON.data[5];
+              //console.log(character['hair_color']);
+              console.log(JSON.stringify(resultJSON.data[5]['hair_color']));
 
+          })
+          .catch(err=>{
+              console.log(err);
+          })
+        },[])
+
+  const name = "balls";
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <div>
+    <Character />
+  </div>
   );
 }
+
+
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,15 @@
-// Write your Character component here
+import React, {usestate, useEffect} from 'react';
+import axios from "axios";
+
+
+class Character extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>Ntttttt</h1>
+        </div>
+      )
+    }
+  }
+
+export default Character
